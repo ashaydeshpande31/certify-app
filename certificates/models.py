10a540dd82c398fe0f2914ecf0b3d817d59eac92
ashaydeshpande31 <ashaydeshpande2025@gmail.com>
2026-08-01 22:44:09 +0530

    Update models.py

--- certificates/models.py
+++ certificates/models.py
@@ -1,6 +1,5 @@
 import uuid
-from django.db import models
-from cloudinary_storage.storage import MediaCloudinaryStorage
+from django.db import modelsfrom cloudinary_storage.storage import MediaCloudinaryStorage, RawMediaCloudinaryStorage
 
 class Event(models.Model):
     """One hackathon / workshop / event for which certificates are generated."""
