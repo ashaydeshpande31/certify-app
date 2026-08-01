64d884c07fc32cd2ac15a4e2b3bbc001e27fe0fe
ashaydeshpande31 <ashaydeshpande2025@gmail.com>
2026-08-01 22:36:45 +0530

    Update settings.py

--- certify_project/settings.py
+++ certify_project/settings.py
@@ -140,11 +140,17 @@ STATICFILES_DIRS = [BASE_DIR / 'certificates' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
+}
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 MEDIA_URL = 'media/'
