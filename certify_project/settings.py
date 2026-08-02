56e32ae86d69af5b100298f0b01763d09e4e89bb
ashaydeshpande31 <ashaydeshpande2025@gmail.com>
2026-08-02 23:39:12 +0530

    Switch to SSL port 465 for more reliable Gmail connection

--- certify_project/settings.py
+++ certify_project/settings.py
@@ -168,8 +168,10 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 if EMAIL_HOST_USER and EMAIL_HOST_PASSWORD:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
+    EMAIL_PORT = 465
+    EMAIL_USE_SSL = True
+    EMAIL_USE_TLS = False
+    EMAIL_TIMEOUT = 30
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
